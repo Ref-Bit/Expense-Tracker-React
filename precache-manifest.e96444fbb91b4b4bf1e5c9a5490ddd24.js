702ca8875b0c8fc365d55751a1f8a15f2bfd61cc
Ref-Bit <rsv.gho@gmail.com>
2020-04-28 16:02:53 +0300

    Updates

---
+++ precache-manifest.e96444fbb91b4b4bf1e5c9a5490ddd24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6726b0d3c576cb221b9c7d5a760e100a",
+    "url": "/Expense-Tracker-React/index.html"
+  },
+  {
+    "revision": "bdd1b09f9968f61ce1b8",
+    "url": "/Expense-Tracker-React/static/css/main.bf3b25ff.chunk.css"
+  },
+  {
+    "revision": "7e72cb9e9e02334dd881",
+    "url": "/Expense-Tracker-React/static/js/2.fd289dce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Expense-Tracker-React/static/js/2.fd289dce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdd1b09f9968f61ce1b8",
+    "url": "/Expense-Tracker-React/static/js/main.d223e841.chunk.js"
+  },
+  {
+    "revision": "40b478976907b6bacab5",
+    "url": "/Expense-Tracker-React/static/js/runtime-main.ef9cffdf.js"
+  }
+]);
